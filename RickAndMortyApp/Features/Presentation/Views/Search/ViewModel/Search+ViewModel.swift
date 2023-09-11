4d40730e262a4480d6e8c42f841838677e8c5435
Cane Allesta <idevapps19@gmail.com>
2023-09-11 12:09:51 +0200

    add some code documentation

--- RickAndMortyApp/Features/Presentation/Views/Search/ViewModel/Search+ViewModel.swift
+++ RickAndMortyApp/Features/Presentation/Views/Search/ViewModel/Search+ViewModel.swift
@@ -10,18 +10,31 @@ import Observation
 import Combine
 
 @Observable class SearchViewModel {
+    /// The use case object for performing character-related operations.
     private var useCase: CharacterUseCase
+    
+    /// The current page number for API pagination.
     private var currentPage: Int = 1
+    
+    /// A flag indicating whether more characters can be fetched.
     private var canFetchMoreCharacters: Bool = true
     
+    /// A flag to indicate whether the API call is in progress.
     var isLoading: Bool = false
+    
+    /// A list to store the fetched character models.
     var characterList: [CharacterBusinessModel] = []
-    var workItem: DispatchWorkItem? 
+    
+    /// A work item for debouncing, if needed.
+    var workItem: DispatchWorkItem?
     
     init(useCase: CharacterUseCase = DefaultCharacterUseCase()) {
         self.useCase = useCase
     }
     
+    /// Searches for characters based on the provided name.
+    ///
+    /// - Parameter name: The name of the character to search for.
     func searchCharacter(by name: String) async {
         if name.isEmpty {
             resetSearch()
@@ -34,6 +47,9 @@ import Combine
         await fetchSearchCharacter(by: name)
     }
     
+    /// Fetches more characters for an ongoing search.
+    ///
+    /// - Parameter name: The name of the character to search for.
     func searchMoreCharacters(by name: String) async {
         if name.isEmpty {
             resetSearch()
@@ -45,13 +61,18 @@ import Combine
     }
 }
 
+// MARK: - Private Methods
 extension SearchViewModel {
+    
+    /// Internal method to fetch characters based on a name.
+    ///
+    /// - Parameter name: The name of the character to search for.
     private func fetchSearchCharacter(by name: String) async {
         do {
             let response = try await useCase.searchCharacter(by: name, and: "\(currentPage)")
             await MainActor.run {
-                characterList += response.results // Append new characters to the existing array
-                currentPage += 1 // Increment the page number
+                characterList += response.results  // Append new characters to existing list
+                currentPage += 1  // Increment page number for next fetch
                 isLoading = false
                 canFetchMoreCharacters = true
             }
@@ -60,12 +81,14 @@ extension SearchViewModel {
         }
     }
     
+    /// Resets the search state to the initial settings.
     private func resetSearch() {
         canFetchMoreCharacters = true
         characterList = []
         currentPage = 1
     }
     
+    /// Handles any errors during API calls.
     private func handleError() async {
         if characterList.isEmpty {
             await MainActor.run {
@@ -79,3 +102,4 @@ extension SearchViewModel {
         }
     }
 }
+
